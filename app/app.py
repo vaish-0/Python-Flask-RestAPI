cee5a85014fe95c85e512e69011df8fa20314ab3
00farhan00 <112959835+00farhan00@users.noreply.github.com>
2023-06-22 11:51:31 +0530

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+try:
+    from API import (app,
+                     api,
+                     HeathController,docs
+
+                     )
+except Exception as e:
+    print("Modules are Missing : {} ".format(e))
+
+api.add_resource(HeathController, '/health_check')
+docs.register(HeathController)
+
